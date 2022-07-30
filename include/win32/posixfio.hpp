b5104c369d6a7909b2306b6da64963f0bda2323d
Parola Marco <mark.prl.97@gmail.com>
2022-07-30 14:57:03 +0200

    Prepared branch for win32 compatibility

---
+++ include/win32/posixfio.hpp
@@ -0,0 +1,159 @@
+#pragma once
+
+extern "C" {
+	#include <fcntl.h>
+}
+
+#include <cstdint>
+#include <new>
+#include <type_traits>
+
+
+
+namespace posixfio {
+
+	#ifdef POSIXFIO_NOTHROW
+		inline namespace no_throw {
+	#endif
+
+
+	using fd_t = int;
+
+	// These aliases are the easiest solution for portability, but they may limit how
+	// functions are implemented for each environment.
+	using mode_t = unsigned;
+	using ssize_t = std::make_signed_t<size_t>;
+	using off_t = ssize_t;
+
+	// Class forward declarations
+	class Errcode;
+	class File;
+	class Pipe;
+	class FileView;
+
+	using Errno = Errcode;  // `errcode` replaces `errno`, since the latter is a macro
+
+
+	struct Errcode {
+		int errcode;
+
+		constexpr Errcode(int errcode): errcode(errcode) { }
+		constexpr operator int() const { return errcode; }
+	};
+
+	struct FileError : public Errcode {
+		fd_t fd;
+
+		constexpr FileError(fd_t fd, int errcode): Errcode(errcode), fd(fd) { }
+		constexpr operator int() = delete;
+	};
+
+
+	class File {
+		friend FileView;
+
+	private:
+		fd_t fd_;
+
+	public:
+		static constexpr fd_t NULL_FD = -1;
+
+		/** POSIX-compliant. */
+		static File open(const char* pathname, int flags, mode_t mode = 00660);
+
+		/** POSIX-compliant. */
+		static File creat(const char* pathname, mode_t mode);
+
+		/** POSIX-compliant. */
+		static File openat(fd_t dirfd, const char* pathname, int flags, mode_t mode = 0);
+
+
+		File();
+		File(fd_t);
+		File(const File&);
+		File(File&&);
+		~File();
+
+		/** Almost POSIX-compliant: returns `false` exclusively when an error occurs. */
+		bool close();
+
+		File& operator=(const File&);
+		File& operator=(File&&);
+
+		/** Sets the internal file descriptor to `NULL_FD`, then returns its old value. */
+		inline fd_t disown() { fd_t r = fd_;  fd_ = NULL_FD;  return r; }
+
+		/** POSIX-compliant. */
+		inline File dup() const { return File(*this); }
+
+		/** POSIX-compliant. */
+		File dup2(fd_t fildes2) const;
+
+		/** POSIX-compliant. */
+		ssize_t read(void* buf, size_t count);
+
+		/** POSIX-compliant. */
+		ssize_t write(const void* buf, size_t count);
+
+		/** POSIX-compliant. */
+		off_t lseek(off_t offset, int whence);
+
+		/** Almost POSIX-compliant: returns `false` exclusively when an error occurs. */
+		bool ftruncate(off_t length);
+
+		/** Almost POSIX-compliant: returns `false` exclusively when an error occurs. */
+		bool fsync();
+
+		/** Almost POSIX-compliant: returns `false` exclusively when an error occurs. */
+		bool fdatasync();
+
+		inline operator bool() const { return fd_ >= 0; }
+		inline bool operator!() const { return ! operator bool(); }
+		inline fd_t fd() const { return fd_; }
+		inline operator fd_t() const { return fd_; }
+	};
+
+
+	struct Pipe {
+		File rd, wr;
+
+		/** POSIX-compliant. */
+		static Pipe create();
+
+		inline Pipe() { }
+		Pipe(const Pipe&) = default;
+		Pipe(Pipe&&) = default;
+		~Pipe() = default;
+
+		Pipe& operator=(const Pipe&) = default;
+		Pipe& operator=(Pipe&&) = default;
+
+		inline ssize_t read(void* buf, size_t count) { return rd.read(buf, count); };
+		inline ssize_t write(void* buf, size_t count) { return wr.write(buf, count); };
+
+		inline operator bool() const { return rd && wr; }
+		inline bool operator!() const { return ! operator bool(); }
+	};
+
+
+	class FileView : public File {
+	public:
+		using File::File;
+		inline FileView(fd_t fd): File(fd) { }
+		inline FileView(const File& f): File(f.fd_) { }
+		inline FileView(const FileView& cp): File(cp.fd_) { }
+		inline ~FileView() { disown(); }
+
+		inline FileView& operator=(fd_t fd) { fd_ = fd;  return *this; }
+		inline FileView& operator=(const File& f) { fd_ = f.fd_;  return *this; }
+		inline FileView& operator=(const FileView& fv) { fd_ = fv.fd_;  return *this; }
+
+		inline bool close() { disown(); return true; }
+	};
+
+
+	#ifdef POSIXFIO_NOTHROW
+		}
+	#endif
+
+}
---
+++ include/win32/posixfio_tl.hpp
@@ -0,0 +1,254 @@
+#pragma once
+
+#include <posixfio.hpp>
+
+#include <utility>
+#include <cstddef>
+#include <cstring>
+
+
+
+namespace posixfio {
+
+	namespace buffer_op {
+
+		/* This namespace is only to be used internally by this library,
+		 * and its signatures may change at any time in any way.
+		 * */
+
+		ssize_t bfRead(FileView, void* buf, size_t* bufBegPtr, size_t* bufEndPtr, void* dst, size_t count);
+		ssize_t bfWrite(FileView, void* buf, size_t* bufBegPtr, size_t* bufEndPtr, size_t bufCapacity, const void* src, size_t count);
+
+	};
+
+
+
+	using byte_t = unsigned char;
+
+
+	/** Repeatedly reads data from the given FileView, until `count`
+	 * bytes have been read, EOF is reached or an error occurs. */
+	ssize_t readAll(FileView, void* buf, size_t count);
+
+	/** Repeatedly reads data from the given FileView, until more than
+	 * `least` and less than `count+1` bytes have been read,
+	 * EOF is reached or an error occurs. */
+	ssize_t readLeast(FileView, void* buf, size_t least, size_t count);
+
+
+	/** Repeatedly writes data to the given FileView, until `count`
+	 * bytes have been written or an error occurs. */
+	ssize_t writeAll(FileView, const void* buf, size_t count);
+
+	/** Repeatedly writes data to the given FileView, until more than
+	 * `least` and less than `count+1` bytes have been written
+	 * bytes have been written or an error occurs. */
+	ssize_t writeLeast(FileView, const void* buf, size_t least, size_t count);
+
+
+	class InputBuffer {
+	private:
+		FileView file_;
+		size_t begin_;
+		size_t end_;
+		size_t capacity_;
+		byte_t* buffer_;
+
+	public:
+		InputBuffer();
+		InputBuffer(const InputBuffer&) = delete;
+		InputBuffer(InputBuffer&&);
+		InputBuffer(FileView, size_t capacity);
+		~InputBuffer();
+
+		InputBuffer& operator=(InputBuffer&&);
+
+		inline const FileView file() const { return file_; }
+
+		/** Similar to File::read, but may fail after a partial read. */
+		ssize_t read(void* buf, size_t count);
+
+		/** Try to fill the buffer, if it isn't already full. */
+		ssize_t fill();
+
+		/** If the buffer is empty, try to fill it; then discard one byte.
+		 * The return value follows File::read semantics. */
+		ssize_t fwd();
+
+		/** Returns a pointer to the first ready-to-read byte in the buffer. */
+		inline byte_t* data() { return buffer_ + begin_; }
+
+		/** Returns a pointer to the first ready-to-read byte in the buffer. */
+		inline const byte_t* data() const { return buffer_ + begin_; }
+
+		/** Returns the number of ready-to-read bytes. */
+		inline size_t size() const { return end_ - begin_; }
+
+		/** Discard the entire buffer; the next read will try to fill the buffer. */
+		inline void discard() { begin_ = 0;  end_ = 0; }
+	};
+
+
+	class OutputBuffer {
+	private:
+		FileView file_;
+		size_t begin_;
+		size_t end_;
+		size_t capacity_;
+		byte_t* buffer_;
+
+	public:
+		OutputBuffer();
+		OutputBuffer(const OutputBuffer&) = delete;
+		OutputBuffer(OutputBuffer&&);
+		OutputBuffer(FileView, size_t capacity);
+		~OutputBuffer();
+
+		OutputBuffer& operator=(OutputBuffer&&);
+
+		inline const FileView file() const { return file_; }
+
+		/** Similar to File::write, but may fail after a partial write. */
+		ssize_t write(const void* buf, size_t count);
+
+		/** Write all the ready-to-write bytes. */
+		void flush();
+	};
+
+
+	template<size_t capacity = 4096>
+	class ArrayInputBuffer {
+		static_assert(capacity > 0);
+	private:
+		FileView file_;
+		size_t bufferBegin_;
+		size_t bufferEnd_;
+		byte_t buffer_[capacity];
+
+	public:
+		ArrayInputBuffer() = default;
+		ArrayInputBuffer(const ArrayInputBuffer&) = delete;
+
+		ArrayInputBuffer(ArrayInputBuffer&& mv):
+				file_(std::move(mv.file_)),
+				bufferBegin_(0),
+				bufferEnd_(std::move(mv.bufferEnd_))
+		{
+			memcpy(buffer_,
+				mv.buffer_,
+				bufferEnd_ - bufferBegin_ );
+		}
+
+		ArrayInputBuffer(FileView file):
+				file_(std::move(file)),
+				bufferBegin_(0),
+				bufferEnd_(0)
+		{ }
+
+		ArrayInputBuffer& operator=(ArrayInputBuffer&& mv) {
+			this->~ArrayInputBuffer();
+			return * new (this) ArrayInputBuffer(std::move(mv));
+		}
+
+		const FileView file() const { return file_; }
+
+		/** Similar to File::read, but may fail after a partial read. */
+		ssize_t read(void* buf, size_t count) {
+			return buffer_op::bfRead(file_, buffer_, &bufferBegin_, &bufferEnd_, buf, count);
+		}
+
+		/** Try to fill the buffer, if it isn't already full. */
+		ssize_t fill() {
+			if(bufferEnd_ < capacity) {
+				ssize_t rd = file_.read(buffer_ + bufferEnd_, capacity - bufferEnd_);
+				if(rd >= 0) [[likely]]  bufferEnd_ += rd;
+				return rd;
+			} else {
+				return 0;
+			}
+		}
+
+		/** Discard the entire buffer; the next read will try to fill the buffer. */
+		inline void discard() { bufferBegin_ = 0;  bufferEnd_ = 0; }
+
+		/** If the buffer is empty, try to fill it; then discard one byte.
+		 * The return value follows File::read semantics. */
+		ssize_t fwd() {
+			if(bufferBegin_ + 1 >= bufferEnd_) {
+				if(bufferEnd_ >= capacity)  discard();
+				ssize_t fl = fill();
+				if(fl <= 0)  return fl;
+			} else {
+				++ bufferBegin_;
+			}
+			return 1;
+		}
+
+		/** Returns a pointer to the first ready-to-read byte in the buffer. */
+		inline byte_t* data() { return buffer_ + bufferBegin_; }
+
+		/** Returns a pointer to the first ready-to-read byte in the buffer. */
+		inline const byte_t* data() const { return buffer_ + bufferBegin_; }
+
+		/** Returns the number of ready-to-read bytes. */
+		inline size_t size() const { return bufferEnd_ - bufferBegin_; }
+	};
+
+
+	template<size_t capacity = 4096>
+	class ArrayOutputBuffer {
+	private:
+		FileView file_;
+		size_t bufferBegin_;
+		size_t bufferEnd_;
+		byte_t buffer_[capacity];
+
+	public:
+		ArrayOutputBuffer() = default;
+		ArrayOutputBuffer(const ArrayOutputBuffer&) = delete;
+
+		ArrayOutputBuffer(ArrayOutputBuffer&& mv):
+				file_(std::move(mv.file_)),
+				bufferBegin_(0),
+				bufferEnd_(std::move(mv.bufferEnd_))
+		{
+			memcpy(buffer_, mv.buffer_, bufferEnd_);
+		}
+
+		ArrayOutputBuffer(FileView file):
+				file_(std::move(file)),
+				bufferBegin_(0),
+				bufferEnd_(0)
+		{ }
+
+		~ArrayOutputBuffer() {
+			if(file_) {
+				if(bufferEnd_ > bufferBegin_) {
+					writeAll(file_,
+						reinterpret_cast<byte_t*>(buffer_) + bufferBegin_,
+						bufferEnd_ - bufferBegin_ );
+				}
+			}
+		}
+
+		ArrayOutputBuffer& operator=(ArrayOutputBuffer&& mv) {
+			this->~ArrayOutputBuffer();
+			return * new (this) ArrayOutputBuffer(std::move(mv));
+		}
+
+		const FileView file() const { return file_; }
+
+		/** Similar to File::write, but may fail after a partial write. */
+		ssize_t write(const void* buf, size_t count) {
+			return buffer_op::bfWrite(file_, buffer_, &bufferBegin_, &bufferEnd_, capacity, buf, count);
+		}
+
+		/** Write all the ready-to-write bytes. */
+		void flush() {
+			writeAll(file_,
+				reinterpret_cast<byte_t*>(buffer_) + bufferBegin_,
+				bufferEnd_ - bufferBegin_ );
+		}
+	};
+
+}
